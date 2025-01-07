d2c55a48753afed0fd141fe29ba1efb2ca27a09a
Michał Skałka <michal.skalka@swmansion.com>
2025-01-07 15:38:25 +0100

    Fix iOS structure for both Fabric and Paper

--- ios/Wallet.h
+++ ios/Wallet.h
@@ -1,6 +1,12 @@
 
-#import "generated/RNWalletSpec/RNWalletSpec.h"
+#ifdef RCT_NEW_ARCH_ENABLED
+#import "RNWalletSpec.h"
 
 @interface Wallet : NSObject <NativeWalletSpec>
+#else
+#import <React/RCTBridgeModule.h>
+
+@interface Wallet : NSObject <RCTBridgeModule>
+#endif
 
 @end
--- ios/Wallet.mm
+++ ios/Wallet.mm
@@ -3,16 +3,25 @@
 @implementation Wallet
 RCT_EXPORT_MODULE()
 
-- (NSNumber *)multiply:(double)a b:(double)b {
+// Example method
+// See // https://reactnative.dev/docs/native-modules-ios
+RCT_EXPORT_METHOD(multiply:(double)a
+                  b:(double)b
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+{
     NSNumber *result = @(a * b);
 
-    return result;
+    resolve(result);
 }
 
+// Don't compile this code when we build for the old architecture.
+#ifdef RCT_NEW_ARCH_ENABLED
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
     (const facebook::react::ObjCTurboModule::InitParams &)params
 {
     return std::make_shared<facebook::react::NativeWalletSpecJSI>(params);
 }
+#endif
 
 @end
