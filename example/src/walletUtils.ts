9392cbac0f452c009310d3608660d728ecfb80e8
Darwin Bracamonte <bracamontedar@gmail.com>
2025-08-29 15:27:52 -0300

    feat(android): add resumeAddCardToGoogleWallet example code

--- example/src/walletUtils.ts
+++ example/src/walletUtils.ts
@@ -1,6 +1,9 @@
 import {
   addCardToAppleWallet,
   addCardToGoogleWallet,
+  listTokens,
+  resumeAddCardToGoogleWallet,
+  type CardStatus,
 } from '@expensify/react-native-wallet';
 import * as CONST from './CONST';
 import {Platform} from 'react-native';
@@ -15,8 +18,26 @@ function issuerEncryptPayloadCallback(
   return Promise.resolve(CONST.IOSDummyEncryptPayload);
 }
 
-async function addCardToWallet() {
+async function addCardToWallet(cardStatus?: CardStatus) {
   if (Platform.OS === 'android') {
+    if (cardStatus === 'requireActivation') {
+      const tokens = await listTokens();
+      const existingToken = tokens.find(
+        token =>
+          token.fpanLastFour === CONST.AndroidDummyResumeCardData.lastDigits,
+      );
+
+      if (!existingToken) {
+        throw new Error(
+          `No se encontró el token para la tarjeta terminada en ${CONST.AndroidDummyResumeCardData.lastDigits}`,
+        );
+      }
+
+      return await resumeAddCardToGoogleWallet({
+        ...CONST.AndroidDummyResumeCardData,
+        tokenReferenceId: existingToken.tokenReferenceId,
+      });
+    }
     return addCardToGoogleWallet(CONST.AndroidDummyCardData);
   } else {
     return addCardToAppleWallet(
